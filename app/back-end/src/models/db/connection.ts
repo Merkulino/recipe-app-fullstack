56817b8c00a6077c734661056d158cf7cecb5c8f
Melqui Brito <Merkulino11@gmail.com>
2023-06-14 16:11:06 -0300

    db connection

--- app/back-end/src/models/db/connection.ts
+++ app/back-end/src/models/db/connection.ts
@@ -4,7 +4,7 @@ const connection = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.MYSQL_ROOT_PASSWORD || '123456',
-  database: 'RECIPES_APP'
+  database: process.env.MYSQL_DATABASE || 'RECIPES_APP'
 });
 
 export default connection;
