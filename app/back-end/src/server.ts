6979c2ffd32a6d1b695e3a78ea543d1a8bc420a9
Melqui Brito <Merkulino11@gmail.com>
2023-06-15 13:14:20 -0300

    Test Database connection async

--- app/back-end/src/server.ts
+++ app/back-end/src/server.ts
@@ -1,7 +1,11 @@
 import app from "./app";
+import connection from "./models/db/connection";
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(
-  `Server is running on PORT: ${PORT}`,
-));
+app.listen(PORT, async () => {
+  console.log(`Server is running on PORT: ${PORT}`)
+  if (await connection.execute('SELECT 1')) {
+    console.log('Database Connected')
+  }
+});
