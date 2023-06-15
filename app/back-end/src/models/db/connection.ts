7b5fcb4e4d8a15c568f64c8025694da672672679
Melqui Brito <Merkulino11@gmail.com>
2023-06-15 13:25:20 -0300

    Config db connection

--- app/back-end/src/models/db/connection.ts
+++ app/back-end/src/models/db/connection.ts
@@ -2,9 +2,11 @@ import mysql from 'mysql2/promise';
 
 const connection = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT as unknown as number || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.MYSQL_ROOT_PASSWORD || '123456',
-  database: process.env.MYSQL_DATABASE || 'RECIPES_APP'
+  database: process.env.MYSQL_DATABASE || 'RECIPES_APP',
+  waitForConnections: true,
 });
 
 export default connection;
